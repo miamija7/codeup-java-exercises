e4d5f43e8a8e74cc1958764481a72aeb88405001
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-27 15:36:45 -0500

    exercise 5 and 6

--- java-i/src/HelloWorld.java
+++ java-i/src/HelloWorld.java
@@ -1,16 +1,28 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
         // Exercise 1
         int myFavoriteNumber = 7;
         System.out.println(myFavoriteNumber);
+
         // Exercise 2
         String myString = "This is a Java String";
         System.out.println(myString);
-        // Exercise 3
+
+        // Exercise 3: M is a char not a string
         // myString = 'M';
-        // Exercise 4
+
+        // Exercise 4: 3.14159 is a double not a string
         // myString = 3.14159;
+
+        // Exercise 5: myNumber is not initialized
+        long myNumber;
+        // System.out.println(myNumber);
+
+        // Exercise 6: 3.14 is a double not a long
+        // myNumber = 3.14;
+
 
     }
 }
