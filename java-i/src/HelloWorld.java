e1f46b531fc2ae05161c80e273cac1dcec89c61c
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-27 15:33:21 -0500

    exercise 3 and 4

--- java-i/src/HelloWorld.java
+++ java-i/src/HelloWorld.java
@@ -1,9 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // Exercise 1
         int myFavoriteNumber = 7;
         System.out.println(myFavoriteNumber);
+        // Exercise 2
         String myString = "This is a Java String";
         System.out.println(myString);
+        // Exercise 3
+        // myString = 'M';
+        // Exercise 4
+        // myString = 3.14159;
+
     }
 }
