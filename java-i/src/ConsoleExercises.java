c7467fab8a86920dc6cb1810c290b204507d3dac
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-28 15:20:28 -0500

    control statements and loops notes

--- java-i/src/ConsoleExercises.java
+++ java-i/src/ConsoleExercises.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 public class ConsoleExercises {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f.%n", pi);
+
         // Exercise 1
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please input a number: ");
+        System.out.print("\nPlease input a number: ");
         int nextInt = scanner.nextInt();
         System.out.println("You entered the number: " + nextInt);
 
         // Exercise 2
         System.out.print("\nEnter three words: ");
-        String  word1 = scanner.next(),
+        String word1 = scanner.next(),
                 word2 = scanner.next(),
                 word3 = scanner.next();
         System.out.format("Word1: %s\nWord 2: %s\nWord 3: %s\n", word1, word2, word3);
@@ -23,16 +26,12 @@ public class ConsoleExercises {
 
         // Exercise 5
         System.out.print("\nGive me a length: ");
-        Integer length = Integer.parseInt(scanner.nextLine());
+        int length = Integer.parseInt(scanner.nextLine());
         System.out.print("Give me a width: ");
-        Integer width = Integer.parseInt(scanner.nextLine());
+        int width = Integer.parseInt(scanner.nextLine());
         System.out.format("Area: %d\nPerimeter: %d", length * width, length * 2 + width * 2);
 
         // Exercise 6
         // scanner.useDelimiter("\n");
-
-
-
-
     }
 }
---
+++ java-i/src/ControlStatementsLecture.java
@@ -0,0 +1,5 @@
+public class ControlStatementsLecture {
+    public static void main(String[] args) {
+
+    }
+}
