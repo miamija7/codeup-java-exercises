888e5c286bf477a7f8a0a6d101a72709c0ae26ab
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-28 16:05:33 -0500

    control statements

---
+++ java-i/src/ControlFlowExercises.java
@@ -0,0 +1,10 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // Exercise 1
+        int i = 5;
+        while(i <= 15){
+            System.out.print(i++ + " ");
+        }
+
+    }
+}
