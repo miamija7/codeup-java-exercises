ef0beda436fbd1b741d66da3b6f5041eca59b962
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-28 12:58:58 -0500

    console io

---
+++ java-i/src/ConsoleExercises.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args){
+        // Exercise 1
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input a number: ");
+        int nextInt = scanner.nextInt();
+        System.out.println(nextInt);
+
+
+    }
+}
