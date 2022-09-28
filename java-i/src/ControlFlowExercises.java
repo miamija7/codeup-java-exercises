e7f2e7c3158f2d17cc0ce0bb521f183d912cd00b
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-28 16:31:20 -0500

    control statements exercise 1

--- java-i/src/ControlFlowExercises.java
+++ java-i/src/ControlFlowExercises.java
@@ -1,10 +1,45 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        // Exercise 1
-        int i = 5;
-        while(i <= 15){
-            System.out.print(i++ + " ");
-        }
+//        // Exercise A
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.print(i++ + " ");
+//        }
+//
+//        // Exercise B
+//        i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i < 100);
+//
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i != -10);
+//
+//        long l = 2;
+//        do {
+//            System.out.println(l);
+//            l *= l;
+//        } while (l < 1000000);
+//
+        // Exercise C
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.print(i + " ");
+//        }
+//
+//        for (int i = 0; i <= 100; i+=2) {
+//            System.out.println(i);
+//        }
+//
+//        for (int i = 100; i >= -10; i-=5) {
+//            System.out.println(i);
+//        }
+//
+//        for (long i = 2; i <= 1000000; i*=i) {
+//            System.out.println(i);
+//        }
 
     }
 }
