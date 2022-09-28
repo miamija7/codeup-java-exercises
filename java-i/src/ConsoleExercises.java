4bbd5fd20adfe996bcffeec5ff67f17ccb85572e
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-28 13:17:42 -0500

    console io exercises

--- java-i/src/ConsoleExercises.java
+++ java-i/src/ConsoleExercises.java
@@ -8,6 +8,19 @@ public class ConsoleExercises {
         int nextInt = scanner.nextInt();
         System.out.println(nextInt);
 
+        // Exercise 2
+        System.out.print("Enter three words: ");
+        String  word1 = scanner.next(),
+                word2 = scanner.next(),
+                word3 = scanner.next();
+        System.out.format("Word1: %s\nWord 2: %s\nWord 3: %s\n", word1, word2, word3);
+
+        // Exercise 3
+        scanner.nextLine();
+        System.out.print("Gimme a Sentence: ");
+        String sentence = scanner.nextLine();
+        System.out.println(sentence);
+
 
     }
 }
