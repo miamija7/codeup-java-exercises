a77b3191b3e613fa0c103db9cb8da5fd1e2a86b1
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-28 15:44:39 -0500

    control statements lecture

--- java-i/src/ControlStatementsLecture.java
+++ java-i/src/ControlStatementsLecture.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class ControlStatementsLecture {
@@ -14,10 +16,17 @@ public class ControlStatementsLecture {
         System.out.print("Order: ");
         String entre = sc.next(),
                 side = sc.next();
-        if(entre.equals("nuggets") && side.equals("fries")) {
-            System.out.println("Alright, completed order!");
-        } else {
-            System.out.println("You messed something up with my order...");
+        // if(entre.equals("nuggets") && side.equals("fries")) {
+        //     System.out.println("Alright, completed order!");
+        // } else {
+        //     System.out.println("You messed something up with my order...");
+        // }
+        System.out.println((entre.equals("nuggies") && side.equals("fries")) ?
+                "Yay! You got my order right!":
+                "Awwww, you messed up my order...");
+
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
         }
     }
 }
