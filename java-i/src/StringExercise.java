97cabe068e82a0aa317245bc6cf730d30056f1e6
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-29 12:20:18 -0500

    string exercises

---
+++ java-i/src/StringExercise.java
@@ -0,0 +1,10 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String  str1 = "We don't need no education",
+                str2 = "We don't need no thought control",
+                str3 = "Check \"this\" out, \"s inside of \"s!",
+                str4 = "In windows, the main drive is usually C:\\",
+                str5 = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.format("%s%n%s%n%s%n%s%n%s%n", str1, str2, str3, str4, str5);
+    }
+}
