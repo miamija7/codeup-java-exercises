48dc28b39ccd821476dbaf8d49386b668b0d2484
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-10-04 14:15:48 -0500

    Object Oriented Programming - Exercises

--- java-ii/src/Person.java
+++ java-ii/src/Person.java
@@ -19,10 +19,31 @@ public class Person {
         System.out.println("Hello there, " + this.name + "!");
     }
 
-    // main
+    // main method
     public static void main(String[] args) {
+        // Exercise 1
         Person person = new Person("Phillis");
         System.out.println(person.getName());
         person.sayHello();
+        person.setName("Mia");
+        person.sayHello();
+
+        // Exercise 2
+        Person person1 = new Person("John");
+        Person person2 = new Person("John");
+        System.out.println(person1.getName().equals(person2.getName()));  // true
+        System.out.println(person1 == person2);   // false
+
+        Person person3 = new Person("John");
+        Person person4 = person3;
+        System.out.println(person3 == person4);     // true
+
+        Person person5 = new Person("John");
+        Person person6 = person5;
+        System.out.println(person5.getName());  // John
+        System.out.println(person6.getName());  // John
+        person6.setName("Jane");
+        System.out.println(person5.getName());  // Jane
+        System.out.println(person6.getName());  // Jane
     }
 }
