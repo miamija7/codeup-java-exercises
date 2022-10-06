88a47ed232cffbec4654a39d5e2850f8a5d780e4
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-10-06 10:57:03 -0500

    java i

---
+++ java-i/src/TernaryTester.java
@@ -0,0 +1,8 @@
+public class TernaryTester {
+        public static void main (String[]args){
+            int x = 5 * 4 % 3;
+            System.out.println(x);
+            System.out.println("Testing" + 1);
+        }
+
+}
