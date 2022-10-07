aa4ec92e38fd205747d106e27651cf603babf740
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-10-07 13:50:05 -0500

    Java abstract and interface

--- java-ii/src/shapes/ShapesTest.java
+++ java-ii/src/shapes/ShapesTest.java
@@ -2,12 +2,13 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Measurable myShape = new Rectangle(4, 5);
+        Measurable myShape = new Rectangle(2, 3);
         System.out.println("====== box1 ======");
         System.out.printf("Area: %.1f%nPerimeter: %.1f%n", myShape.getArea(), myShape.getPerimeter());
 
-        myShape = new Square(5);
+        myShape = new Square(2);
         System.out.println("\n====== box2 ======");
         System.out.printf("Area: %.1f%nPerimeter: %.1f%n", myShape.getArea(), myShape.getPerimeter());
+        // System.out.printf("Length: %.1f%Width: %.1f%n", myShape.getLength(), myShape.getWidth());
     }
 }
