e1d6e295d3d97ff7eb05566d7371e633e4c0fedc
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-10-13 14:06:09 -0500

    try/catch exercise

--- java-ii/src/Tests.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-public class Tests {
-    public static void main(String[] args) {
-        test2 emp = new test2("Peter Gibbons", "IT");
-        System.out.println(emp.getName() + " works in " + emp.getDepartment());
-    }
-}
