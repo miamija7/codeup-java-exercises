a2af5ce1a117a52e674159a7fa86acfe958cad6e
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-11-03 13:58:10 -0500

    javabeans exercise

---
+++ java-iii/src/javabeans/models/Author.java
@@ -0,0 +1,63 @@
+package javabeans.models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Author implements Serializable {
+    // Variables
+    private long id;
+    private String first_name;
+    private String last_name;
+    private List<Quote> quotes;
+
+    // Constructors
+    public Author(){};
+    public Author(long id, String first_name, String last_name) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.quotes = new ArrayList<>();
+    }
+    public Author(long id, String first_name, String last_name, List<Quote> quotes) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.quotes = quotes;
+    }
+
+    // Methods
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public List<Quote> getQuotes() {
+        return quotes;
+    }
+
+    public void setQuotes(List<Quote> quotes) {
+        this.quotes = quotes;
+    }
+
+    public String getName(){
+        return this.first_name + " " + this.getLast_name();
+    }
+}
---
+++ java-iii/src/javabeans/models/Pet.java
@@ -0,0 +1,74 @@
+package javabeans.models;
+
+import java.util.Date;
+import java.util.List;
+
+public class Pet {
+    // Variables
+    private long id;
+    private String breed;
+    private String name;
+    private Date birthdate;
+    private List<String> vaccinations;
+    private Employee vet;
+
+    // Constrictors
+    public Pet(){};
+    public Pet(long id, String breed, String name, Date birthdate, List<String> vaccinations, Employee vet) {
+        this.id = id;
+        this.breed = breed;
+        this.name = name;
+        this.birthdate = birthdate;
+        this.vaccinations = vaccinations;
+        this.vet = vet;
+    }
+
+    // Methods
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public List<String> getVaccinations() {
+        return vaccinations;
+    }
+
+    public void setVaccinations(List<String> vaccinations) {
+        this.vaccinations = vaccinations;
+    }
+
+    public Employee getVet() {
+        return vet;
+    }
+
+    public void setVet(Employee vet) {
+        this.vet = vet;
+    }
+}
